b00ff4463f5b4298928d4bd78f872c44d2b9172a
Andreas Trawoeger <atrawog@gmail.com>
2025-01-13 18:58:12 +0100

    Enhance setpassword.sh to include echo statements for git email configuration

--- config/setpassword.sh
+++ config/setpassword.sh
@@ -17,10 +17,11 @@ if [ -n "$PASSWORD" ]; then
 fi
 
 if [ -n "$GIT_USERNAME" ]; then
- echo "Setting git username to $GIT_USERNAME"
+  echo "Setting git username to $GIT_USERNAME"
   git config --global user.name "$GIT_USERNAME"
 fi
 
 if [ -n "$GIT_EMAIL" ]; then
+  echo "Setting git email to $GIT_EMAIL"
   git config --global user.email "$GIT_EMAIL"
 fi
